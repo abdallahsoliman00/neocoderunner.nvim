db52ee990c0fbe813ef500176ade3ca2e4c0e742
Abdallah Soliman <abdallahsoliman03@gmail.com>
2026-06-27 14:14:50 +0300

    Change module name for correct setup

--- lua/neocoderunner/init.lua
+++ lua/neocoderunner/init.lua
@@ -7,7 +7,7 @@ M.config = default_config
 M.setup = function(user_config)
     M.config = vim.tbl_deep_extend("force", default_config, user_config or {})
 
-    require("neocoderunner.commands").setup()
+    require("neocoderunner.user_commands").setup()
 end
 
 return M
